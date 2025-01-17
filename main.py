bcf49a133052a3bf4655cb0681db9fa2c4cedd4e
IhTiYaR0 <ytpolyus@gmail.com>
2025-01-17 06:21:13 +0500

    Update 1

---
+++ main.py
@@ -0,0 +1,49 @@
+import subprocess
+
+packages = \
+["swaybg", #Основные пакеты!
+"kiity",
+"waybar",
+"fish",
+"wofi",
+"git",
+"thunar",
+"gtk2",
+"gtk3",
+"gtk4",
+"neofetch",
+"w3m",
+#Шрифты !
+"ttf-dejavu",
+"ttf-font-awesome",
+"ttf-liberation",
+"ttf-dejavu ",
+"powerline-fonts",
+"noto-fonts",
+"ttf-fira-code",
+"ttf-hack",
+"terminus-font",
+"adobe-source-code-pro-fonts",
+"otf-font-awesome",
+"ttf-jetbrains-mono",
+"nerd-fonts-complete",
+"ttf-fira-code-nerd",
+"powerline-fonts",
+"ttf-google-fonts-git",
+"nerd-fonts",
+"fc-cache -fv",
+"",
+"",
+"inetutils",
+"pkgfile",
+"base-devel",
+"",
+"",
+"",
+"",
+
+]
+
+
+for packag in  packages:
+    subprocess.run(["sudo", "pacman", "-S", "--noconfirm", packag])
