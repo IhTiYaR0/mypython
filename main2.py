dd857ba898aa59cae06a8166ec3fe88267e39e8b
IhTiYaR0 <ytpolyus@gmail.com>
2025-01-17 06:22:05 +0500

    Update 2

--- main2.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-lkjlkjlkjlkjlkjsalkjdlkasjdlksajkjhdfkaladfjklhsfglkjayhsdfliuhyao
-
-subprocess.run(['git', 'clone', 'https://aur.archlinux.org/yay.git'])
-subprocess.run(['makepkg', '-si'], cwd='yay')
-subprocess.run(['rm', '-rf', 'yay'])
